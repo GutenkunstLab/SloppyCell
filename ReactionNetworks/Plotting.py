b64a4322f6f78cfa22cc10ec1454cef8a1f4b6d0
Ryan Gutenkunst <jepetto@users.sourceforge.net>
2005-06-29 18:51:41 +0000

    Now we print the name of a variable if we can, as opposed to always printing the id.

--- ReactionNetworks/Plotting.py
+++ ReactionNetworks/Plotting.py
@@ -1,20 +1,18 @@
-
 import scipy
 from pylab import *
 
-import SloppyCell.Plotting
-PlotEigenvalueSpectrum = SloppyCell.Plotting.PlotEigenvalueSpectrum
+from SloppyCell.Plotting import *
 
 def PlotEigenvectors(eigVects, net = None, title = None):
     nEv = 3
     nOv = len(eigVects[:,0])
     for jj in range(nEv):
-        pylab.subplot(nEv, 1, jj+1)
+        subplot(nEv, 1, jj+1)
         if jj == 0 and title is not None:
-            pylab.title(title)
+            title(title)
 
-        pylab.bar(range(nOv), eigVects[:,jj]/scipy.linalg.norm(eigVects[:,jj]))
-        pylab.axis([-1, nOv] + pylab.axis()[2:])
+        bar(range(nOv), eigVects[:,jj]/scipy.linalg.norm(eigVects[:,jj]))
+        axis([-1, nOv] + axis()[2:])
 
         if net is not None:
             mags = zip(abs(eigVects[:,jj]), range(nOv), eigVects[:,jj])
@@ -24,45 +22,45 @@ def PlotEigenvectors(eigVects, net = None, title = None):
                 name = net.optimizableVars[index].name
                 if name is None:
                     name = net.optimizableVars[index].id
-                pylab.text(index, val + scipy.sign(val)*0.05, 
+                text(index, val + scipy.sign(val)*0.05, 
                            name,
                            horizontalalignment='center',
                            verticalalignment='center')
 
-        a = pylab.axis()
+        a = axis()
         a[0:2] = [-.03*nOv, nOv*1.03]
         a[2] -= 0.1
         a[3] += 0.1
-        pylab.axis(a)
+        axis(a)
 
 def plotStateSpaceTrajectoriesForVariables(traj, id1, id2, thresholds = None):
     xx = traj.getVariableTrajectory(id1)
     yy = traj.getVariableTrajectory(id2)
-    pylab.plot(xx, yy)
-    pylab.plot([xx[0]], [yy[0]], 'or')
-    pylab.xlabel(id1)
-    pylab.ylabel(id2)
+    plot(xx, yy)
+    plot([xx[0]], [yy[0]], 'or')
+    xlabel(id1)
+    ylabel(id2)
     if thresholds is not None:
-        a = pylab.axis()
-        pylab.vlines([thresholds[0]], a[2], a[3])
-        pylab.hlines([thresholds[1]], a[0], a[1])
+        a = axis()
+        vlines([thresholds[0]], a[2], a[3])
+        hlines([thresholds[1]], a[0], a[1])
 
 def plotTrajectoriesForVariables(traj, ids = None, showLegend = True):
     if ids is None:
         ids = traj.net.variables.keys()
 
-    cW = SloppyCell.Plotting.ColorWheel()
+    cW = ColorWheel()
 
     lines = []
     legend = []
     for id in ids:
-        line = pylab.plot(traj.timepoints, traj.getVariableTrajectory(id), 
+        line = plot(traj.timepoints, traj.getVariableTrajectory(id), 
                           cW.next()[::2])
         lines.append(line)
         legend.append(id)
 
     if showLegend:
-        pylab.legend(lines, legend)
+        legend(lines, legend)
 
 
 def PlotTrajectoriesForExperiments(model, experiments, params = None, with_data=True,
@@ -92,7 +90,7 @@ def PlotTrajectoriesForExperiments(model, experiments, params = None, with_data=
 
     model.GetCalculationCollection().Calculate(varsByCalc, params)
     calcVals = model.GetCalculationCollection().GetResults(varsByCalc)
-    cW = SloppyCell.Plotting.ColorWheel()
+    cW = ColorWheel()
     for exptName in experiments:
         expt = exptColl[exptName]
         dataByCalc = expt.GetData()
@@ -101,7 +99,7 @@ def PlotTrajectoriesForExperiments(model, experiments, params = None, with_data=
                 fmt = cW.next()
                 if with_data:
                     for time, (data, error) in dataByCalc[calc][chem].items()[::skip]:
-                        pylab.errorbar(time, data, yerr=error, fmt=fmt,
+                        errorbar(time, data, yerr=error, fmt=fmt,
                                          ecolor=fmt, capsize=6)
 
                 predicted = scipy.array(calcVals[calc][chem].items())
@@ -109,18 +107,18 @@ def PlotTrajectoriesForExperiments(model, experiments, params = None, with_data=
                 predicted = scipy.take(predicted, order)
                 predicted[:,1] = predicted[:,1] *\
                         model.GetScaleFactors()[exptName][chem]
-                lines.append(pylab.plot(predicted[:,0], predicted[:,1],
+                lines.append(plot(predicted[:,0], predicted[:,1],
                                           fmt[::2], linewidth = 3))
                 legend.append(chem + ' in ' + str(calc))# + ' for ' + str(exptName))
 
 
     if showLegend:
-        pylab.legend(lines, legend, loc=4)
+        legend(lines, legend, loc=4)
 
 def PlotDataForExperiments(model, experiments, skip = 1):
     exptColl = model.GetExperimentCollection()
 
-    cW = SloppyCell.Plotting.ColorWheel()
+    cW = ColorWheel()
     for exptName in experiments:
         expt = exptColl[exptName]
         dataByCalc = expt.GetData()
@@ -133,22 +131,55 @@ def PlotDataForExperiments(model, experiments, skip = 1):
                         in enumerate(dataByCalc[calc][chem].items()[::skip]):
                     d[ii] = [time, data, error]
 
-                pylab.errorbar(d[:,0], d[:,1], yerr=d[:,2], fmt=fmt[:-1],
-                                 ecolor=fmt, capsize=6)
+                errorbar(d[:,0], d[:,1], yerr=d[:,2], fmt=fmt[:-1],
+                         ecolor=fmt, capsize=6)
 
 def plot_model_data(model, expts = None, style = 'errorbars',
                     show_legend = True, loc = 'upper left'):
+    """
+    Plot the data in the given experiments for the given model.
+    
+    Note: You may need to run a Plotting.show() to display the plot.
+    
+    Inputs:
+      model: Model whose experiments to plot
+      expts: List of experiment IDs to plot
+      style: Style of plot. Currently supported options are:
+          'errorbars': Plots points and bars for each data point
+          'lines': Plots a continuous line for the data
+      show_legend: Boolean that control whether or not to show the legend
+      loc: Location of the legend. See help(Plotting.legend) for options.
+    """
     plot_model_results(model, expts, style, show_legend, loc, 
                        plot_trajectories = False)
 
 def plot_model_results(model, expts = None, style='errorbars',
                        show_legend = True, loc = 'upper left',
                        plot_data = True, plot_trajectories = True):
+    """
+    Plot the fits to the given experiments for the last cost evalution of the
+    model.
+    
+    Note: You may need to run a Plotting.show() to display the plot.
+    
+    Inputs:
+      model: Model whose results to plot
+      expts: List of experiment IDs to plot, if None is specified, all 
+             experiments are plotted
+      style: Style of plot. Currently supported options are:
+              'errorbars': Plots points and bars for each data point
+              'lines': Plots a continuous line for the data
+      show_legend: Boolean that control whether or not to show the legend
+      loc: Location of the legend. See help(Plotting.legend) for options.
+      plot_data: Boolean that controls whether the data is plotted
+      plot_trajectories: Boolean that controls whether the trajectories are
+                         plotted
+    """
     exptColl = model.GetExperimentCollection()
     calcColl = model.GetCalculationCollection()
 
     lines, labels = [], []
-    cW = SloppyCell.Plotting.ColorWheel()
+    cW = ColorWheel()
     
     if expts is None:
         expts = exptColl.keys()
@@ -156,10 +187,14 @@ def plot_model_results(model, expts = None, style='errorbars',
     for exptId in expts:
         expt = exptColl[exptId]
         dataByCalc = expt.GetData()
+        # We sort the calculation names for easier comparison across plots
         sortedCalcIds = dataByCalc.keys()
         sortedCalcIds.sort()
         for calcId in sortedCalcIds:
-            traj = getattr(calcColl[calcId], 'trajectory', None)
+            # Pull the trajectory from that calculation, defaulting to None
+            #  if it doesn't exist.
+            net = calcColl[calcId]
+            traj = getattr(net, 'trajectory', None)
             for dataId, dataDict in dataByCalc[calcId].items():
                 fmt = cW.next()
 
@@ -169,15 +204,16 @@ def plot_model_results(model, expts = None, style='errorbars',
                                      in dataDict.items()])
                     if style is 'errorbars':
                         l = errorbar(d[:,0], d[:,1], yerr=d[:,2], 
-                                           fmt=fmt[:-1], ecolor=fmt, capsize=6)
+                                     fmt=fmt[:-1], ecolor=fmt, capsize=6)
                     elif style is 'lines':
+                        # Make sure we order the data before plotting
                         order = scipy.argsort(d[:,0], 0)
                         d = scipy.take(d, order, 0)
                         l = plot(d[:,0], d[:,1], fmt[::2])
 
                 if plot_trajectories:
                     if traj is None:
-                        print 'No trajectory for calculation %s!' % calcId
+                        print 'No trajectory in calculation %s!' % calcId
                         print 'The cost must be evaluated before the results',
                         print 'can be plotted.'
                         return
@@ -186,12 +222,20 @@ def plot_model_results(model, expts = None, style='errorbars',
                     result = scaleFactor*traj.getVariableTrajectory(dataId)
                     plot(traj.timepoints, result, fmt[::2], linewidth=3)
 
+                    # We superimpose a dotted black line to distinguish
+                    #  theory from data in this case
                     if style is 'lines':
                         plot(traj.timepoints, result, 'k--', linewidth=3,
                                    zorder = 10)
                 
                 lines.append(l)
-                labels.append('%s in %s for %s' % (dataId, calcId, exptId))
+                # Let's print the pretty name for our variable if we can.
+                name = net.variables.getByKey(dataId).name
+                if name is not None:
+                    printedName = name
+                else:
+                    printedName = dataId
+                labels.append('%s in %s for %s' % (printedName, calcId, exptId))
 
     if show_legend:
         legend(lines, labels, loc=loc)
