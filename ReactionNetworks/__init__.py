e37e6ed62d3fb075afd5d94f8fad2fcc9e5b1b2d
Ryan Gutenkunst <jepetto@users.sourceforge.net>
2005-07-01 15:41:02 +0000

    Broke imports with last __init__.py commit. This should fix it, although the import SloppyCell.Model as M //  M.Model() problem remains.

--- ReactionNetworks/__init__.py
+++ ReactionNetworks/__init__.py
@@ -1,3 +1,7 @@
-from Network import Network
 import Plotting
 import Reactions
+
+# This is the same voodoo that's used in SloppyCell/__init__.py
+if not locals().has_key('Network_mod'):
+    import Network as Network_mod
+Network = Network_mod.Network
--- __init__.py
+++ __init__.py
@@ -1,5 +1,18 @@
 _TEMP_DIR = '.SloppyCell'
 
-from SloppyCell.Collections import KeyedList, ExperimentCollection, Experiment,\
-        CalculationCollection
-from SloppyCell.Model import Model
+import SloppyCell.Collections
+KeyedList = SloppyCell.Collections.KeyedList
+ExperimentCollection = SloppyCell.Collections.ExperimentCollection
+Experiment = SloppyCell.Collections.Experiment
+CalculationCollection = SloppyCell.Collections.CalculationCollection
+
+# This bit of voodoo is to expose the Model class while also allowing the
+#  module it's a part of to be reloaded. This preserves the convenience
+#  of reload() for debugging while also making it easier to expose the Model
+#  class.
+#
+# Note that code that wants to import SloppyCell.Model and get the module still
+#  breaks.
+if not locals().has_key('Model_mod'):
+    import Model as Model_mod
+Model = Model_mod.Model
