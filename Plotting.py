6ac591f56b48e863d1cbd211df0a03f856b9fe06
Ryan Gutenkunst <jepetto@users.sourceforge.net>
2005-07-12 21:33:41 +0000

    Added a plot_eigenvect function.

--- Plotting.py
+++ Plotting.py
@@ -1,4 +1,5 @@
-import pylab, scipy
+import scipy
+from pylab import *
 
 def ColorWheel():
     """
@@ -27,11 +28,38 @@ def PlotEigenvalueSpectrum(vals, lab = None):
     while sym[0] == 'r':
         sym = cW.next()
 
-    line = pylab.semilogy(posRange, posVals, sym[:2], label = lab)
+    line = semilogy(posRange, posVals, sym[:2], label = lab)
     if len(negVals) > 0:
-        pylab.semilogy(negRange, negVals, 'r'+sym[1])
+        semilogy(negRange, negVals, 'r'+sym[1])
 
-    a = pylab.axis()
-    pylab.axis([-3, len(vals)+2, a[2], a[3]])
+    a = axis()
+    axis([-3, len(vals)+2, a[2], a[3]])
 
     return line
+
+def plot_eigenvect(vect, labels=None, num_label = 5):
+    """
+    Plot a given eigenvector.
+
+    If a list of labels is passed in, the largest (in magnitude) num_label bars
+     will be labeled on the plot.
+    """
+    # The 0.4 centers the bars on their numbers, accounting for the default
+    #  bar width of 0.8
+    bar(scipy.arange(-0.4, len(vect) - 0.4, 1), vect/scipy.linalg.norm(vect))
+    a = axis()
+    a[0:2] = [-.03*len(vect), len(vect)*1.03]
+
+    if labels is not None:
+        mags = zip(abs(vect), range(len(vect)), vect)
+        mags.sort()
+        mags.reverse()
+        for mag, index, val in mags[:num_label]:
+            name = labels[index]
+            text(index, val + scipy.sign(val)*0.05, name,
+                 horizontalalignment='center', verticalalignment='center')
+
+        a[2] -= 0.1
+        a[3] += 0.1
+
+    axis(a)
