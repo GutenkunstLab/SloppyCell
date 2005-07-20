c546fdc35b720801ce6fddb0e18c7a5a4256482e
Ryan Gutenkunst <jepetto@users.sourceforge.net>
2005-07-20 13:59:35 +0000

    Added support for messages in tests.
    These are printed afterwords to, for example, tell the user to check the output LaTeX sources for prettiness.

--- Testing/test.py
+++ Testing/test.py
@@ -5,12 +5,19 @@ def run_all_tests():
     all_tests = unittest.TestSuite()
 
     testfiles = glob.glob('test_*.py')
+    mesgs = []
     for file in testfiles:
         module = file[:-3]
         mod = __import__(file[:-3])
         all_tests.addTest(mod.suite)
+        if hasattr(mod, 'message'):
+            mesgs.append(mod.message)
 
     unittest.TextTestRunner(verbosity=2).run(all_tests)
+
+    print
+    for mesg in mesgs:
+        print mesg
 
 if __name__ == '__main__':
     run_all_tests()
