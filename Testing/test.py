e6ae953da62830332601b6188f8a46a5d16fc7ff
Ryan Gutenkunst <jepetto@users.sourceforge.net>
2005-07-29 19:51:24 +0000

    Read messages *after* tests have run.

--- Testing/test.py
+++ Testing/test.py
@@ -6,15 +6,19 @@ def run_all_tests():
 
     testfiles = glob.glob('test_*.py')
     mesgs = []
+    all_test_mods = []
     for file in testfiles:
         module = file[:-3]
         mod = __import__(file[:-3])
         if hasattr(mod, 'suite'):
+            all_test_mods.append(mod)
             all_tests.addTest(mod.suite)
-        if hasattr(mod, 'message'):
-            mesgs.append(mod.message)
 
     unittest.TextTestRunner(verbosity=2).run(all_tests)
+
+    for mod in all_test_mods:
+        if hasattr(mod, 'message'):
+            mesgs.append(mod.message)
 
     print
     for mesg in mesgs:
