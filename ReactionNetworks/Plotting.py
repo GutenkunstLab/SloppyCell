1415c547cebeb92ac722729cf5d2c029c17b324b
Ryan Gutenkunst <jepetto@users.sourceforge.net>
2005-08-10 19:25:01 +0000

    Added plot_ensemble_results

--- ReactionNetworks/Plotting.py
+++ ReactionNetworks/Plotting.py
@@ -1,8 +1,10 @@
+import sets
+
 import scipy
 from pylab import *
 
 from SloppyCell.Plotting import *
-
+import Network
 
 def PlotEigenvectors(eigVects, net = None, title = None):
     nEv = 3
@@ -176,8 +178,8 @@ def plot_model_results(model, expts = None, style='errorbars',
       plot_trajectories: Boolean that controls whether the trajectories are
                          plotted
     """
-    exptColl = model.GetExperimentCollection()
-    calcColl = model.GetCalculationCollection()
+    exptColl = model.get_expts()
+    calcColl = model.get_calcs()
 
     lines, labels = [], []
     cW = ColorWheel()
@@ -241,6 +243,123 @@ def plot_model_results(model, expts = None, style='errorbars',
     if show_legend:
         legend(lines, labels, loc=loc)
 
+def plot_ensemble_results(model, ensemble, expts = None, 
+                          style='errorbars',
+                          show_legend = True, loc = 'upper left',
+                          plot_data = True, plot_trajectories = True):
+    """
+    Plot the fits to the given experiments over an ensemble. 
+
+    Note that this recalculates the cost for every member of the ensemble, so
+     it may be very slow. Filtering correlated members from the ensemble is
+     strongly recommended.
+    
+    Inputs:
+      model: Model whose results to plot
+      ensemble: Parameter ensemble
+      expts: List of experiment IDs to plot, if None is specified, all 
+             experiments are plotted
+      style: Style of plot. Currently supported options are:
+              'errorbars': Plots points and bars for each data point
+              'lines': Plots a continuous line for the data
+      show_legend: Boolean that control whether or not to show the legend
+      loc: Location of the legend. See help(Plotting.legend) for options.
+      plot_data: Boolean that controls whether the data is plotted
+      plot_trajectories: Boolean that controls whether the trajectories are
+                         plotted
+    """
+    exptColl = model.get_expts()
+    nets = model.get_calcs()
+
+    if expts is None:
+        expts = exptColl.keys()
+
+    lines, labels = [], []
+    cW = ColorWheel()
+
+    Network.Network.pretty_plotting()
+    model.cost(ensemble[0])
+    timepoints = {}
+    for netId, net in nets.items():
+        traj = getattr(net, 'trajectory', None)
+        if traj is not None:
+            net.times_to_add = traj.timepoints
+            timepoints[netId] = net.times_to_add
+
+    Network.Network.full_speed()
+
+    results = {}
+    for params in ensemble:
+        model.cost(params)
+        for exptId in expts:
+            expt = exptColl[exptId]
+            results.setdefault(exptId, {})
+            dataByCalc = expt.GetData()
+            for netId in dataByCalc.keys():
+                results[exptId].setdefault(netId, {})
+                # Pull the trajectory from that calculation, defaulting to None
+                #  if it doesn't exist.
+                net = nets[netId]
+                traj = net.trajectory
+                for dataId in dataByCalc[netId].keys():
+                    results[exptId][netId].setdefault(dataId, [])
+
+                    scaleFactor = model.GetScaleFactors()[exptId][dataId]
+                    result = scaleFactor*traj.getVariableTrajectory(dataId)
+                    results[exptId][netId][dataId].append(result)
+
+    for exptId in expts:
+        expt = exptColl[exptId]
+        dataByCalc = expt.GetData()
+        # We sort the calculation names for easier comparison across plots
+        sortedCalcIds = dataByCalc.keys()
+        sortedCalcIds.sort()
+        for netId in sortedCalcIds:
+            for dataId, dataDict in dataByCalc[netId].items():
+                fmt = cW.next()
+
+                if plot_data:
+                    # Pull the data out of the dictionary and into an array
+                    d = scipy.array([[t, v, e] for (t, (v, e))
+                                     in dataDict.items()])
+                    if style is 'errorbars':
+                        l = errorbar(d[:,0], d[:,1], yerr=d[:,2], 
+                                     fmt=fmt[:-1], ecolor='k', capsize=6)[0]
+                    elif style is 'lines':
+                        # Make sure we order the data before plotting
+                        order = scipy.argsort(d[:,0], 0)
+                        d = scipy.take(d, order, 0)
+                        l = plot(d[:,0], d[:,1], fmt[::2])
+
+                if plot_trajectories:
+                    times = timepoints[netId]
+                    mean_vals = scipy.mean(results[exptId][netId][dataId], 0)
+                    std_vals = scipy.std(results[exptId][netId][dataId], 0)
+
+                    lower_vals = mean_vals - std_vals
+                    upper_vals = mean_vals + std_vals
+
+                    # Plot the polygon
+                    xpts = scipy.concatenate((times, times[::-1]))
+                    ypts = scipy.concatenate((lower_vals, upper_vals[::-1]))
+                    fill(xpts, ypts, fmt[0], alpha=0.4)
+
+                lines.append(l)
+                # Let's print the pretty name for our variable if we can.
+                name = net.variables.getByKey(dataId).name
+                if name is not None:
+                    printedName = name
+                else:
+                    printedName = dataId
+                labels.append('%s in %s for %s' % (printedName, netId, exptId))
+
+    if show_legend:
+        legend(lines, labels, loc=loc)
+
+    for net in nets.values():
+        net.times_to_add = None
+
+
 def plot_trajectory(traj, vars = None,
                     show_legend = True, loc = 'upper left',
                     logx = False, logy = False):
@@ -264,25 +383,44 @@ def plot_trajectory(traj, vars = None,
     if show_legend:
         legend(loc=loc)
 
-
-def plot_ensemble_trajs(best=None, mean=None, lower=None, upper=None,
+def plot_ensemble_trajs(best_traj=None, mean_traj=None, 
+                        std_traj=None, std_devs=1.0,
                         vars=None, 
-                        show_legend = True, loc = 'upper left',):
+                        show_legend = True, loc = 'upper left'):
+    """
+    Plot the results of a Network ensemble.
+
+    Inputs:
+     best_traj -- Best-fit trajectory
+     mean_traj -- Mean trajectory
+     std_traj -- Trajectory of standard deviations
+     std_devs -- Number of standard deviations to draw bounds at
+     vars -- List of variable ids to plot the bounds for
+
+     show_legend -- Boolean to show a legend or not
+     loc -- Location code for legend
+    """
     cW = ColorWheel()
     lines = []
     labels = []
     for var in vars:
         fmt = cW.next()
-        if best is not None:
-            l = plot(best.timepoints, best.getVariableTrajectory(var), 
+        if best_traj is not None:
+            l = plot(best_traj.timepoints, best_traj.getVariableTrajectory(var),
                      fmt[0]+'-', linewidth=2)
-        if mean is not None:
-            plot(mean.timepoints, mean.getVariableTrajectory(var), fmt[0]+'--', 
-                 linewidth=2)
-        if upper is not None and lower is not None:
-            xpts = scipy.concatenate((lower.timepoints, upper.timepoints[::-1]))
-            ypts = scipy.concatenate((lower.getVariableTrajectory(var),
-                                      upper.getVariableTrajectory(var)[::-1]))
+        if mean_traj is not None:
+            times = mean_traj.timepoints
+            mean_vals = mean_traj.getVariableTrajectory(var)
+            plot(times, mean_vals, fmt[0]+'--', linewidth=2)
+        if mean_traj is not None and std_traj is not None and std_devs > 0:
+            # Calculate the bounds
+            std_vals = std_traj.getVariableTrajectory(var)
+            lower_vals = mean_vals - std_devs*std_vals
+            upper_vals = mean_vals + std_devs*std_vals
+
+            # Plot the polygon
+            xpts = scipy.concatenate((times, times[::-1]))
+            ypts = scipy.concatenate((lower_vals, upper_vals[::-1]))
             fill(xpts, ypts, fmt[0], alpha=0.4)
 
         lines.append(l)
