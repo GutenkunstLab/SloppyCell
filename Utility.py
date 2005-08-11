283db1b20bd398ef8482a3192bb0cc49fc60e021
Ryan Gutenkunst <jepetto@users.sourceforge.net>
2005-08-11 18:27:32 +0000

    Added send_email

--- Utility.py
+++ Utility.py
@@ -1,8 +1,28 @@
-import scipy
 import cPickle
+import smtplib
+from email.MIMEText import MIMEText
+
+import scipy
 
 eig = scipy.linalg.eig
 linspace = scipy.linspace
+
+def send_email(to_addr, from_addr, subject, message):
+    """
+    Send a plain-text email to a single address.
+    """
+    # Create a text/plain message
+    msg = MIMEText(message)
+    msg['Subject'] = subject
+    msg['To'], msg['From'] = to_addr, from_addr
+
+    # From the documentation for email... Not sure what it means. :-)
+    ## Send the message via our own SMTP server, but don't include the
+    ## envelope header.
+    s = smtplib.SMTP()
+    s.connect()
+    s.sendmail(from_addr, [to_addr], msg.as_string())
+    s.close()
 
 def save(obj, filename):
     """
