1b69ad9d0b506e5afc6dac2c0f07bcc6b041c2ab
Ryan Gutenkunst <jepetto@users.sourceforge.net>
2005-08-29 19:54:33 +0000

    Added Dynamics, PerfectData imports

--- ReactionNetworks/__init__.py
+++ ReactionNetworks/__init__.py
@@ -1,10 +1,14 @@
 from SloppyCell import *
 
+import Dynamics
 import IO
 import Plotting
 import Reactions
+import PerfectData
 
 # This is the same voodoo that's used in SloppyCell/__init__.py
-if not locals().has_key('Network_mod'):
-    import Network as Network_mod
-Network = Network_mod.Network
+classimps = ['Network']
+for modname in classimps:
+    if not locals().has_key(modname+'_mod'):
+        exec 'import %s as %s_mod' % (modname, modname)
+    exec '%s = %s_mod.%s' % (modname, modname, modname)
