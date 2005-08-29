46b14fe9d6cd0142c06109ce3d528c9eeb4bb207
Ryan Gutenkunst <jepetto@users.sourceforge.net>
2005-08-29 19:59:52 +0000

    Cleaned-up now that we have observers

--- Optimization.py
+++ Optimization.py
@@ -3,40 +3,36 @@ import sys
 
 import scipy
 
-def fmin_powell(m, params, print_costs=False, *args, **kwargs):
-    if print_costs:
-        def func(log_params):
-            c = m.cost_log_params(log_params)
-            print 'Cost: %g' % c
-            sys.stdout.flush()
-            return c
-    else:
-        func = m.cost_log_params
+from KeyedList import KeyedList
 
-    pmin = scipy.optimize.fmin_powell(func, scipy.log(params), *args, **kwargs)
-    pout = copy.deepcopy(params)
-    pout.update(scipy.exp(pmin))
+def fmin_powell_log_params(m, params, *args, **kwargs):
+    func = m.cost_log_params
+
+    pmin = scipy.optimize.fmin_powell(func, scipy.log(params), 
+                                      *args, **kwargs)
+    if isinstance(params, KeyedList):
+        pout = params.copy()
+        pout.update(scipy.exp(pmin))
+
     return pout
 
-def fmin(m, params, print_costs=False, *args, **kwargs):
-    if print_costs:
-        def func(log_params):
-            c = m.cost_log_params(log_params)
-            print 'Cost: %g' % c
-            sys.stdout.flush()
-            return c
-    else:
-        func = m.cost_log_params
+def fmin_log_params(m, params, *args, **kwargs):
+    func = m.cost_log_params
 
-    pmin = scipy.optimize.fmin(func, scipy.log(params), *args, **kwargs)
-    pout = copy.deepcopy(params)
-    pout.update(scipy.exp(pmin))
+    pmin = scipy.optimize.fmin(func, scipy.log(params),
+                               *args, **kwargs)
+
+    if isinstance(params, KeyedList):
+        pout = params.copy()
+        pout.update(scipy.exp(pmin))
     return pout
 
-def leastsq(m, params, *args, **kwargs):
+def leastsq_log_params(m, params, *args, **kwargs):
     func = m.res_log_params
 
     pmin, msg = scipy.optimize.leastsq(func, scipy.log(params), *args, **kwargs)
-    pout = copy.deepcopy(params)
-    pout.update(scipy.exp(pmin))
+
+    if isinstance(params, KeyedList):
+        pout = params.copy()
+        pout.update(scipy.exp(pmin))
     return pout
