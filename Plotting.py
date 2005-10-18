dc996176e03f06730363b2f82719c01b2256dba8
Ryan Gutenkunst <jepetto@users.sourceforge.net>
2005-10-18 19:42:44 +0000

    Clean-up of plot_eigvals

--- Plotting.py
+++ Plotting.py
@@ -24,23 +24,34 @@ def ColorWheel(colors = ('b', 'g', 'r', 'c', 'm', 'k'),
                 for c in colors:
                    yield c + s + l
 
-eigvals_cW = ColorWheel(colors = ('b', 'g', 'c', 'm', 'k'), 
+vals_cW = ColorWheel(colors = ('b', 'r', 'g', 'c', 'm', 'y', 'k'), 
                         lines = None)
-def plot_eigvals(vals, label=None, offset = 0):
-    posVals = abs(scipy.compress(vals > 0, vals))
-    posRange = scipy.compress(vals > 0, range(len(vals)))
-    negVals = abs(scipy.compress(vals < 0, vals))
-    negRange = scipy.compress(vals < 0, range(len(vals)))
+def plot_eigvals(vals, label=None, offset=0, indicate_neg=True, join=False):
+    posVals = abs(scipy.compress(scipy.real(vals) > 0, vals))
+    posRange = scipy.compress(scipy.real(vals) > 0, range(len(vals)))
+    negVals = abs(scipy.compress(scipy.real(vals) < 0, vals))
+    negRange = scipy.compress(scipy.real(vals) < 0, range(len(vals)))
 
-    sym = eigvals_cW.next()
-    line = semilogy(posRange+offset, posVals, sym, label = label)
-    if len(negVals) > 0:
-        semilogy(negRange+offset, negVals, 'r'+sym[1:])
+    sym = vals_cW.next()
+    if indicate_neg:
+        if len(negVals) > 0:
+            semilogy(negRange+offset, negVals, 'r'+sym[1:], zorder=1)
+        if sym[0] == 'r':
+            sym = vals_cW.next()
+
+    line = semilogy(posRange+offset, posVals, sym, label = label, zorder=0)
+
+    if join:
+        plot(scipy.arange(len(vals)) + offset, abs(vals), sym[0]+'--', 
+             zorder=-1)
 
     a = axis()
     axis([-0.05*len(vals) + offset, 1.05*(len(vals) - 1) + offset, a[2], a[3]])
 
     return line
+
+def plot_singvals(vals, label=None, offset=0, join=False):
+    return plot_eigvals(vals, label, offset, indicate_neg=False, join=join)
 
 PlotEigenvalueSpectrum = plot_eigvals
 
@@ -53,6 +64,7 @@ def plot_eigvect(vect, labels=None, num_label = 5):
     """
     # The 0.4 centers the bars on their numbers, accounting for the default
     #  bar width of 0.8
+    vect = scipy.real(vect)
     bar(scipy.arange(len(vect)) - 0.4, vect/scipy.linalg.norm(vect))
     a = axis()
     a[0:2] = [-.03*len(vect) - 0.4, (len(vect) - 1)*1.03 + 0.4]
