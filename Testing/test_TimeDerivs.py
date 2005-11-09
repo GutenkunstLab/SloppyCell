bcd64f727ad69db36e957d71e92e078ca9661a95
fpc2 <fpc2@users.sourceforge.net>
2005-11-09 16:42:15 +0000

    test routine for time derivs of trajectory and sensitivity trajectory, when Dynamics.return_derivs = True

---
+++ Testing/test_TimeDerivs.py
@@ -0,0 +1,40 @@
+import unittest
+
+import scipy,copy
+import matplotlib.mlab as mlab
+from SloppyCell.ReactionNetworks import *
+
+from TestNetwork import net
+net = copy.deepcopy(net)
+net.setInitialVariableValue('A', 1.0)
+net.setInitialVariableValue('B', 2.0)
+net.addParameter('xIC',4.0)
+net.setInitialVariableValue('x','2.0*exp(xIC)')
+net.compile()
+
+
+class test_TimeDerivs(unittest.TestCase):
+    def test_TimeDerivs(self):
+        """Test time derivatives of the trajectory and the sensitivity trajectory """
+        Dynamics.return_derivs = True
+        net.add_int_times = False
+        # need really finely spaced time points for the finite differencing to work
+        Traj = Dynamics.integrate(net,scipy.linspace(0.0,.001,1000))
+        sensTraj = Dynamics.integrate_sensitivity(net,scipy.linspace(0.0,.001,1000))
+        x_vals = Traj.getVariableTrajectory('x')
+        x_vals_deriv = scipy.diff(x_vals)/scipy.diff(Traj.timepoints)
+        relerror = mlab.norm( x_vals_deriv-Traj.getVariableTrajectory(('x','time'))[:-1] )/mlab.norm(x_vals_deriv)
+
+        x_vals_wrtA = sensTraj.getVariableTrajectory(('x','A'))
+        x_vals_wrtA_deriv = scipy.diff(x_vals_wrtA)/scipy.diff(sensTraj.timepoints)
+
+        relerror2 = mlab.norm( x_vals_wrtA_deriv-sensTraj.getVariableTrajectory(('x','A','time'))[:-1] )\
+                    /mlab.norm(x_vals_wrtA_deriv)
+
+        self.assertAlmostEqual(relerror, 0.0, 5,'Failed on time deriv of x in Traj')
+        self.assertAlmostEqual(relerror2, 0.0, 5,'Failed on time deriv of sensitivity of x wrt A in sensTraj')
+
+suite = unittest.makeSuite(test_TimeDerivs)
+
+if __name__ == '__main__':
+    unittest.main()
