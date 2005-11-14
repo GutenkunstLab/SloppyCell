d5bf02801c666700485f1a726a14604bb1ec0435
fpc2 <fpc2@users.sourceforge.net>
2005-11-14 17:01:54 +0000

    add a PriorInLog residual to the model before the test

--- Testing/test_DerivativeCalculators.py
+++ Testing/test_DerivativeCalculators.py
@@ -4,11 +4,15 @@ import unittest
 import scipy
 
 from SloppyCell.ReactionNetworks import *
-
+import SloppyCell
 import TestNetwork
 
 net = TestNetwork.net
-m = TestNetwork.m
+m = copy.deepcopy(TestNetwork.m)
+# the following is useful to see if prior derivatives are computed
+# correctly
+m.AddResidual(SloppyCell.Residuals.PriorInLog('priorOnA', 'A',
+	        scipy.log(1.1),scipy.log(2.0) ) )
 
 class test_DerivativeCalculators(unittest.TestCase):
     def test_GetJandJtJ(self):
