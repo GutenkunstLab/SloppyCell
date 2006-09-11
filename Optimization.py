8cf8cf96f6f6a22d7825ef9857140ebf2a45a926
Ryan Gutenkunst <jepetto@users.sourceforge.net>
2006-09-11 20:07:51 +0000

    fmin_log_params now catches SloppyCellExceptions

--- Optimization.py
+++ Optimization.py
@@ -4,6 +4,7 @@ import sys
 import scipy
 
 import SloppyCell.KeyedList_mod as KeyedList_mod
+import SloppyCell.Utility as Utility
 KeyedList = KeyedList_mod.KeyedList
 
 def fmin_powell_log_params(m, params, *args, **kwargs):
@@ -19,6 +20,12 @@ def fmin_powell_log_params(m, params, *args, **kwargs):
         return scipy.exp(pmin)
 
 def fmin_log_params(m, params, *args, **kwargs):
+    def func(log_params):
+        try:
+            return m.cost_log_params(log_params)
+        except Utility.SloppyCellException:
+            return scipy.inf
+
     func = m.cost_log_params
 
     pmin = scipy.optimize.fmin(func, scipy.log(params),
