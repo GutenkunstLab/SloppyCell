e5e236fe13b01d5396773c72be326d539bd73464
Ryan Gutenkunst <jepetto@users.sourceforge.net>
2006-09-20 19:29:43 +0000

    Workaround to get sdist working on AFS systems

--- setup.py
+++ setup.py
@@ -3,6 +3,17 @@ try:
 except ImportError:
     import numpy.distutils.core as core
 
+# This is a kludge so that sdist works on AFS systems.
+# AFS doesn't allow hard-linking across directories, but if we're on linux
+#  we still have a os.link function (which is all distutils check for).
+# If we delete os.link, sdist will copy rather than link.
+# See http://mail.python.org/pipermail/distutils-sig/2002-October/002990.html
+#  for more detail, and a patch that apparently never got applied to trunk
+#  distutils
+# I don't know why this kludge wasn't needed for Python 2.3
+import os
+del os.link
+
 lsodar = core.Extension(name = 'SloppyCell._lsodar',
                         sources = ['lsodar.pyf', 'odepack/opkdmain.f', 
                                    'odepack/opkda1.f', 'odepack/opkda2.f'])
