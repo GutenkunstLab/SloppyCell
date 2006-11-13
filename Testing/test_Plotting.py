63bc1c6a544c4893305dad3e2202283898e23e9a
Ryan Gutenkunst <jepetto@users.sourceforge.net>
2006-11-13 15:13:57 +0000

    Tests don't fail if plotting fails, just print a warning. (like missing libsbml)

--- Testing/test_Plotting.py
+++ Testing/test_Plotting.py
@@ -14,7 +14,18 @@ class test_Plotting(unittest.TestCase):
         """Ensure plotting basic results doesn't crash"""
         Plotting.plot_model_results(m)
 
-suite = unittest.makeSuite(test_Plotting)
-
+# Only run if Plotting available
+import SloppyCell
+_HAVE_PLOTTING = hasattr(SloppyCell, 'Plotting')
+no_plotting_msg = 'Plotting not available.'
+if _HAVE_PLOTTING:
+    suite = unittest.makeSuite(test_Plotting)
+else:
+    message = no_plotting_msg
+    
 if __name__ == '__main__':
-    unittest.main()
+    if _HAVE_PLOTTING:
+        unittest.main()
+    else: 
+        print no_plotting_msg
+
