4baf79241b6cf4fa77e905de1656220e2070a795
Jordan Atlas <jcatlas@users.sourceforge.net>
2007-02-28 17:29:42 +0000

    This is the first version of test_AlgebraicRules.py.  Includes the following tests: """ Basic test of Algebraic Rules """ """ Test that algebraic rhs functions that are functions of assignment rules are parsed correctly"""

---
+++ Testing/test_AlgebraicRules.py
@@ -0,0 +1,107 @@
+import unittest
+import os
+
+import scipy
+import SloppyCell.Utility as Utility
+from SloppyCell.ReactionNetworks import *
+import SloppyCell.daskr
+
+try:
+    from SloppyCell.daskr import daeint
+    _HAVE_DASKR = True
+except ImportError:
+    _HAVE_DASKR = False
+
+
+redir = Utility.Redirector()
+
+################################################################################
+
+# Load the fast reaction example from the SBML semantic test suite.
+file_path = os.path.join('SBML_files',
+                         'algebraicRules-fastReactionExample-l2.xml')
+algebraic_net = IO.from_SBML_file(file_path,
+                                  'algebraicRules_fastReactionExample')
+tlist_algebraic_net = scipy.array([0] + [0.8*x for x in range(1, 51)])
+
+
+################################################################################
+
+# Load a similar algebraic rule where assignment variables are used in the rule.
+file_path = os.path.join('SBML_files',
+                         'algebraicRules-assignment_in_algebraic.xml')
+algebraic_net_assignment = IO.from_SBML_file(file_path,
+                                  'algebraicRules-assignment_in_algebraic')
+
+
+
+
+
+
+################################################################################
+
+
+class test_AlgebraicRules(unittest.TestCase):
+    def test_basic(self):
+        """ Basic test of Algebraic Rules """
+        algebraic_traj = Dynamics.integrate_J(algebraic_net, tlist_algebraic_net)
+
+        self.assertAlmostEqual(algebraic_traj.get_var_val('X0',4.8), 0.618783392, 5)
+        self.assertAlmostEqual(algebraic_traj.get_var_val('X1',21.6), 0.653837775, 5)
+        self.assertAlmostEqual(algebraic_traj.get_var_val('T', 29.6), 0.138253942, 5)
+        self.assertAlmostEqual(algebraic_traj.get_var_val('S1', 40.0), 0.018207409, 5)
+        self.assertAlmostEqual(algebraic_traj.get_var_val('S2', 16.8), 0.210750878, 5)
+
+    def test_assignment_in_algebraic(self):
+        """ Test that algebraic rhs functions that are functions of assignment \
+rules are parsed correctly"""
+
+        # integrate using the same time points as the base case
+        algebraic_traj = Dynamics.integrate_J(algebraic_net_assignment, tlist_algebraic_net)
+
+        # make sure that the correct variables were identified as algebraic
+
+        alg_vars = algebraic_net_assignment.algebraicVars
+    
+        self.assertEqual(alg_vars.has_key('S1'), True)
+        self.assertEqual(alg_vars.has_key('T'), False)
+        self.assertEqual(alg_vars.has_key('S_sum'), False)
+        self.assertEqual(alg_vars.has_key('Alg_Rule_RHS'), False)
+
+        # p_1 is a paramter that was set to be non-constant, but which is
+        # implicitly constant because no equations modify it.  We need to check to
+        # make sure it's not being identified as an algebraic variable.
+        self.assertEqual(alg_vars.has_key('p_1'), False)
+
+        # p_2 is modified by an event assignment, so it should not be identified
+        # as an algebraic variable.
+        self.assertEqual(alg_vars.has_key('p_2'), False)
+        
+
+        # finally, we check to make sure the algebraic variables list is the correct
+        # length
+
+        self.assertEqual(len(alg_vars), 1)
+
+        self.assertAlmostEqual(algebraic_traj.get_var_val('X0',4.8), 0.618783392, 5)
+        self.assertAlmostEqual(algebraic_traj.get_var_val('X1',21.6), 0.653837775, 5)
+        self.assertAlmostEqual(algebraic_traj.get_var_val('T', 29.6), 0.138253942, 5)
+        self.assertAlmostEqual(algebraic_traj.get_var_val('S1', 40.0), 0.018207409, 5)
+        self.assertAlmostEqual(algebraic_traj.get_var_val('S2', 16.8), 0.210750878, 5)
+
+
+
+################################################################################
+
+        
+no_daskrr_msg = 'daskr not working!'
+if _HAVE_DASKR:
+    suite = unittest.makeSuite(test_AlgebraicRules)
+else:
+    message = no_daskrr_msg
+
+if __name__ == '__main__':
+    if _HAVE_DASKR:
+        unittest.main()
+    else:
+        print no_daskrr_msg
