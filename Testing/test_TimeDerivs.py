735aadfe9d6a8014e6b7398d5272599b7ba6ed73
Ryan Gutenkunst <jepetto@users.sourceforge.net>
2007-05-09 16:49:03 +0000

    Update to newer SloppyCell API calls.

--- Testing/test_TimeDerivs.py
+++ Testing/test_TimeDerivs.py
@@ -1,10 +1,10 @@
 import unittest
 
-import scipy,copy
+import scipy
 from SloppyCell.ReactionNetworks import *
 
 from TestNetwork import net
-net = copy.deepcopy(net)
+net = net.copy()
 net.setInitialVariableValue('A', 1.0)
 net.setInitialVariableValue('B', 2.0)
 net.addParameter('xIC',4.0)
@@ -14,23 +14,30 @@ net.compile()
 
 class test_TimeDerivs(unittest.TestCase):
     def test_TimeDerivs(self):
-        """Test time derivatives of the trajectory and the sensitivity trajectory """
+        """Test time derivatives of the trajectory and the sensitivity
+        trajectory """
         net.add_int_times = False
-        # need really finely spaced time points for the finite differencing to work
-        Traj = Dynamics.integrate(net,scipy.linspace(0.0,.001,1000), return_derivs=True)
-        sensTraj = Dynamics.integrate_sensitivity(net,scipy.linspace(0.0,.001,1000), return_derivs=True)
-        x_vals = Traj.getVariableTrajectory('x')
-        x_vals_deriv = scipy.diff(x_vals)/scipy.diff(Traj.timepoints)
-        relerror = scipy.linalg.norm( x_vals_deriv-Traj.getVariableTrajectory(('x','time'))[:-1] )/scipy.linalg.norm(x_vals_deriv)
+        # need really finely spaced time points for the finite differencing to
+        # work
+        times = scipy.linspace(0.0,.001,1000)
+        Traj = Dynamics.integrate(net, times, return_derivs=True)
+        sensTraj = Dynamics.integrate_sensitivity(net, times, 
+                                                  return_derivs=True)
 
-        x_vals_wrtA = sensTraj.getVariableTrajectory(('x','A'))
-        x_vals_wrtA_deriv = scipy.diff(x_vals_wrtA)/scipy.diff(sensTraj.timepoints)
+        x_vals = Traj.get_var_traj('x')
+        x_vals_deriv = scipy.diff(x_vals)/scipy.diff(Traj.get_times())
+        relerror = scipy.linalg.norm(x_vals_deriv-Traj.get_var_traj(('x','time'))[:-1])/scipy.linalg.norm(x_vals_deriv)
 
-        relerror2 = scipy.linalg.norm( x_vals_wrtA_deriv-sensTraj.getVariableTrajectory(('x','A','time'))[:-1] )\
+        x_vals_wrtA = sensTraj.get_var_traj(('x','A'))
+        x_vals_wrtA_deriv = scipy.diff(x_vals_wrtA)/scipy.diff(sensTraj.get_times())
+
+        relerror2 = scipy.linalg.norm(x_vals_wrtA_deriv-sensTraj.get_var_traj(('x','A','time'))[:-1])\
                     /scipy.linalg.norm(x_vals_wrtA_deriv)
 
-        self.assertAlmostEqual(relerror, 0.0, 5,'Failed on time deriv of x in Traj')
-        self.assertAlmostEqual(relerror2, 0.0, 5,'Failed on time deriv of sensitivity of x wrt A in sensTraj')
+        self.assertAlmostEqual(relerror, 0.0, 5, 'Failed on time deriv of x '
+                               'in Traj')
+        self.assertAlmostEqual(relerror2, 0.0, 5, 'Failed on time deriv of '
+                               'sensitivity of x wrt A in sensTraj')
 
 suite = unittest.makeSuite(test_TimeDerivs)
 
