d3b04a572e0774a6a66feb1d30746e455956bf81
Ryan Gutenkunst <jepetto@users.sourceforge.net>
2007-05-11 21:08:20 +0000

    Slight change to ensure testing w/o C compilation.

--- Testing/test.py
+++ Testing/test.py
@@ -1,10 +1,13 @@
 import glob
 import unittest
 
+import SloppyCell
 import SloppyCell.Utility
 import SloppyCell.ReactionNetworks.Network_mod 
 
 def run_all_tests():
+    # This is to run all the tests with C compilation initiall disabled.
+    SloppyCell.ReactionNetworks.Network_mod.Network.disable_c = True
     all_tests = unittest.TestSuite()
 
     testfiles = glob.glob('test_*.py')
@@ -19,14 +22,13 @@ def run_all_tests():
 
     SloppyCell.Utility.disable_warnings()
     print '*' * 80
-    print 'Running tests with C compilation enabled.'
-    print '*' * 80
-    SloppyCell.ReactionNetworks.Network_mod.disable_c = True
-    unittest.TextTestRunner(verbosity=2).run(all_tests)
-    print '*' * 80
     print 'Running tests with C compilation disabled.'
     print '*' * 80
-    SloppyCell.ReactionNetworks.Network_mod.disable_c = False
+    unittest.TextTestRunner(verbosity=2).run(all_tests)
+    print '*' * 80
+    print 'Running tests with C compilation enabled.'
+    print '*' * 80
+    SloppyCell.ReactionNetworks.Network_mod.Network.disable_c = False
     unittest.TextTestRunner(verbosity=2).run(all_tests)
 
     for mod in all_test_mods:
