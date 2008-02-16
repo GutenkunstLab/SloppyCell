99b37feb6bacc3edf341d35d579434606641ad69
Jordan Atlas <jcatlas@users.sourceforge.net>
2008-02-16 19:20:59 +0000

    Added new tests to catch problems with outputting stoichiometry math terms.

--- Testing/test_SBMLInterface.py
+++ Testing/test_SBMLInterface.py
@@ -1,5 +1,6 @@
 import unittest
 import os
+import copy
 
 import scipy
 import SloppyCell.Utility as Utility
@@ -9,6 +10,8 @@ from SloppyCell.ReactionNetworks import *
 # To avoid extra dependencies on libsbml, we use verions built by SloppyCell.
 from AlgTestNets import algebraic_net, algebraic_net_assignment, algebraic_net_multi
 tlist_algebraic_net = scipy.array([0] + [0.8*x for x in range(1, 51)])
+
+sbml_file = os.path.join('SBML_files','algebraicRules-fastReactionExample-l2.xml')
 
 class test_SBMLInterface(unittest.TestCase):
     def test_tofile(self):
@@ -23,6 +26,51 @@ class test_SBMLInterface(unittest.TestCase):
         SBMLInterface.toSBMLFile(algebraic_net, 'algebraic_net.xml')
 
         self.assertEqual(os.path.exists('algebraic_net.xml'), True)
+
+    def test_fromfile(self):
+        """ Basic test of SBML input """
+
+        fastReaction_net = SBMLInterface.fromSBMLFile(sbml_file)
+        
+        self.assertEqual(fastReaction_net.id, 'algebraicRules_fastReactionExample')
+
+    def test_stoichiometryPreservation(self):
+        """ Test that products with stoichiometryMath set are output correctly. """
+        SBMLInterface.toSBMLFile(algebraic_net, 'algebraic_net.xml')
+        local_alg_net = SBMLInterface.fromSBMLFile('algebraic_net.xml')
+
+        out_rxn = local_alg_net.reactions.get('out')
+        self.assertEqual(out_rxn.product_stoichiometry['X1'], ['1 / 1'])
+
+    def test_changeStoichiometry_SloppyCellModel(self):
+        """ Test that products with stoichiometryMath set are output correctly. """
+        SBMLInterface.toSBMLFile(algebraic_net, 'algebraic_net.xml')
+
+        local_alg_net = copy.deepcopy(algebraic_net)
+        out_rxn = local_alg_net.reactions.get('out')
+
+        out_rxn.change_stoichiometry('T', -2)
+        out_rxn.change_stoichiometry('X1', '2 / 2')
+        
+        self.assertEqual(out_rxn.reactant_stoichiometry, None)
+        self.assertEqual(out_rxn.product_stoichiometry, None)
+        self.assertEqual(out_rxn.stoichiometry['T'], -2)
+        self.assertEqual(out_rxn.stoichiometry['X1'], '2 / 2')        
+
+    def test_changeStoichiometry_SBMLModel(self):
+        """ Test that products with stoichiometryMath set are output correctly. """
+        SBMLInterface.toSBMLFile(algebraic_net, 'algebraic_net.xml')
+        local_alg_net = SBMLInterface.fromSBMLFile('algebraic_net.xml')
+
+        out_rxn = local_alg_net.reactions.get('out')
+
+        out_rxn.change_stoichiometry('T', -2)
+        out_rxn.change_stoichiometry('X1', '2 / 2')
+        
+        self.assertEqual(out_rxn.product_stoichiometry['T'], [-2])
+        self.assertEqual(out_rxn.product_stoichiometry['X1'], ['2 / 2'])
+        self.assertEqual(out_rxn.stoichiometry['T'], -2)
+        self.assertEqual(out_rxn.stoichiometry['X1'], '2 / 2')  
 
 
 ################################################################################
