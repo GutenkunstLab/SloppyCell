8768a7dceb5b9398f08142a97b0628f16a3cd127
Ryan Gutenkunst <jepetto@users.sourceforge.net>
2010-04-16 17:30:25 +0000

    Add process id to generated module name, to prevent collisions on multicore machines.

--- ReactionNetworks/Network_mod.py
+++ ReactionNetworks/Network_mod.py
@@ -3174,11 +3174,13 @@ class Network:
         logger.debug('Outputting C for network %s.' % self.get_id())
         if mod_name is None:
             semi_unique = str(time.time())
-            mod_name = '%s_%i_%s_%s' % (self.get_id(), SloppyCell.my_rank,
-                                        SloppyCell.my_host, semi_unique[::-1])
+            mod_name = '%s_%i_%s_%s_%s' % (self.get_id(), SloppyCell.my_rank,
+                                           SloppyCell.my_host,semi_unique[::-1],
+                                           os.getpid())
             mod_name = mod_name.replace('-', '_')
             # f2py fails if base filename has a '.' in it.
             mod_name = mod_name.replace('.', '_')
+
 
         # Write the C code to a file.
         c_fd = open('%s.c' % mod_name, 'w')
