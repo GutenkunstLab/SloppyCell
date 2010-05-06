3afde52a4eb52a13b749b647fbfb70166d781b20
Ryan Gutenkunst <jepetto@users.sourceforge.net>
2010-05-06 03:47:37 +0000

    Protect test_Plotting from broken cost evaluation.

--- Testing/test_Plotting.py
+++ Testing/test_Plotting.py
@@ -3,15 +3,15 @@ import unittest
 from SloppyCell.ReactionNetworks import *
 from TestNetwork import net, m, params
 
-m.cost(params)
-
 class test_Plotting(unittest.TestCase):
     def test_data_plot(self):
         """Ensure plotting basic data doesn't crash"""
+        m.cost(params)
         Plotting.plot_model_data(m)
 
     def test_results_plot(self):
         """Ensure plotting basic results doesn't crash"""
+        m.cost(params)
         Plotting.plot_model_results(m)
 
 # Only run if Plotting available
