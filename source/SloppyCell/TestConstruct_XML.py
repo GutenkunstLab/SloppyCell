Fixed mysterious residual adding bug & added Hessian routine

source/SloppyCell/TestConstruct_XML.py
@@ -248,13 +248,17 @@ def prior_drier(root, model):
                         for param_id,value in param_list:
                             val_p = float(value)
                             x = float(width)
-                            prior_dictionary[param_id] = {'val': val_p, 'x': x}
+                            model.AddResidual(Residuals.PriorInLog('prior_on_%s' % param_id, param_id, scipy.log(val_p),
+                                                                   scipy.log(x)))
+                            prior_dictionary[param_id] = (val_p, x)
                     else:
-                        #TODO: This needs to be fixed
+                        # TODO: This needs to be fixed
                         param = all_params[all_params.index(parameter)]
                         val_p = param.items()[1]
                         x = width
-                        prior_dictionary[parameter] = {'val': val_p, 'x': x}
+                        model.AddResidual(Residuals.PriorInLog('prior_on_%s' % param_id, param_id, scipy.log(val_p),
+                                                               scipy.log(x)))
+                        prior_dictionary[parameter] = (val_p,x)
                 except KeyError:
                     lower = float(bounds['lower'])
                     upper = float(bounds['upper'])
@@ -263,11 +267,14 @@ def prior_drier(root, model):
                     x = sqrt(upper / val_p)
                     # Just in case something goes wrong but doesn't cause any blatant errors
                     assert (x == sqrt(val_p / lower))
-                    prior_dictionary[parameter] = {'val': val_p, 'x': x}
+                    model.AddResidual(Residuals.PriorInLog('prior_on_%s' % param_id, param_id, scipy.log(val_p),
+                                                           scipy.log(x)))
+                    prior_dictionary[parameter] = (val_p,x)
     except KeyError as a:
         logger.warn('Prior Calculation malfunction: ensure prior bounds are numbers, and do not include things like'
                     '"log(x)", "sin(x)", "sqrt()"')
         logger.warn(a)
+
     return prior_dictionary
 
 # Action routines begin here
@@ -277,10 +284,8 @@ def prior_drier(root, model):
 def add_residuals(root, model):
     root = root.find("Parameters")
     prior_dictionary = prior_drier(root, model)
-    for param in prior_dictionary.keys():
-        values = prior_dictionary[param]
-        res = Residuals.PriorInLog(param + '_prior', param, log(values['val']), log(values['x']))
-        model.AddResidual(res)
+
+
 
 
 def set_ic(fit_root, net):
@@ -415,13 +420,19 @@ def cost_lm(params, m, optimize=True, plot=False, initial_cost = False, order = 
             pass
     if optimize:
         order.reverse()
+        new_params = params.copy()
+
+
         for opt in order:
+            print opt
             routine_dict_n = routine_dict_drier(opt.attrib)
             try:
                 opt_type = routine_dict_n.pop('type').lower()
             except KeyError:
                 opt_type = 'levenburg-marquardt'
-            params = optimization_dictionary[opt_type](m, params, **kwargs)
+
+            new_params = optimization_dictionary[opt_type](m, new_params, **routine_dict_n)
+            print new_params
         optimized_cost = m.cost(params)
         print 'Optimized cost:', optimized_cost
         # print 'Optimized parameters:', params
@@ -434,7 +445,8 @@ def cost_lm(params, m, optimize=True, plot=False, initial_cost = False, order = 
         Plotting.figure()
         Plotting.plot_model_results(m)
         Plotting.title('After Optimization')
-    return_dictionary["params"] = params
+    return_dictionary["params"] = new_params
+    print return_dictionary
     return return_dictionary
 
 
@@ -805,9 +817,32 @@ def trajectory_integration(result_dictionary, current_root, network_dictionary, 
                 padding = max(max_list) * .1
                 Plotting.axis([int(lower_bound), int(upper_bound), min(min_list), max(max_list)+padding])
 
+@check_to_save
+def hessian(routine_dict, result_dictionary, model, network_dictionary, **kwargs):
+    try:
+        optimized_params = result_dictionary['optimization']['params']
+    except KeyError:
+        optimized_params = kwargs['params']
+    try:
+        hess = kwargs['loaded_object']
+    except KeyError:
+        try:
+            eps = routine_dict.pop('eps')
+        except KeyError:
+            eps = 0.0001
+        try:
+            if routine_dict.pop('log'):
+                print eps
+                hess = model.hessian_log_params(optimized_params, eps, **routine_dict)
+        except KeyError:
+            hess = model.hessian(optimized_params, eps, **routine_dict)
+    evals, evects = Utility.eig(hess)
+    Plotting.figure()
+    Plotting.plot_eigvals(evals)
+    Plotting.figure()
+    Plotting.plot_eigvect(evects[:,0], optimized_params.keys())
+    return hess
 
-def hessian(routine_dict, result_dictionary, current_root, network_dictionary, **kwargs):
-    pass
 
 
 @check_to_save
@@ -846,7 +881,8 @@ def make_happen(root, experiment, xml_file=None, file_name=None, sbml_reference 
     result_dictionary = dict()
     network_dictionary = dict()
     action_function_dictionary = {'optimization': optimization, 'ensemble': ensemble, 'histogram': histogram_r,
-                           'ensembletrajectories': ensemble_traj, 'trajectory': trajectory_integration}
+                                  'ensembletrajectories': ensemble_traj, 'trajectory': trajectory_integration,
+                                  'hessian':hessian}
     network_func_dictionary = {'set_constant': set_constant, 'add_species': add_species,
                                'add_assignment': add_assignment, 'set_optimizable': set_optimizable,
                                'set_initial': set_initial, 'start_fixed': start_fixed,
@@ -906,6 +942,7 @@ def make_happen(root, experiment, xml_file=None, file_name=None, sbml_reference 
     else:
         # If undefined, we default to making a model out of all available experiments and models
         model = Model(experiment.values(), network_dictionary.values())
+
     try:
         fit_root = root.find("Parameters").find("Fit")
         set_ic(fit_root, net)
@@ -914,6 +951,8 @@ def make_happen(root, experiment, xml_file=None, file_name=None, sbml_reference 
         # No parameters were specified, so we just take them all
         params = model.get_params()
     # This function is standalone and just tacks a residual onto anything that needs it
+
+
     add_residuals(root, model)
 
     print network_dictionary

source/SloppyCell/example/Tyson_1991/runExample.py
@@ -18,18 +18,7 @@ traj1 = Dynamics.integrate(base_net, [0, 100])
 traj2 = Dynamics.integrate(perturbed_net, [0,  100])
 traj3 = Dynamics.integrate(growth_net, [0, 500])
 traj4 = Dynamics.integrate(my_net, [0, 100])
-# print traj2.values
-# print traj2.assignedVarKeys
-# a = []
-# b = []
-# for thing in traj2.values:
-#     a.append(thing[2])
-#     b.append(thing[len(thing)-1])
-# print min(a)
-# print min(b)
-# print traj2.keys()
-# print traj2.dynamicVarKeys
-#
+
 # Figure 3 from the paper.
 Plotting.figure(1, figsize=(15, 4))
 Plotting.subplot(1, 3, 1)
@@ -46,7 +35,6 @@ Plotting.plot_trajectory(traj3, ['M', 'YT'])
 Plotting.axis([0, 500, 0, 0.6])
 
 Plotting.savefig('3.png', dpi=150)
-Plotting.show()
 
 #
 # Now let's fit the model to some data we've defined in Expts.py
@@ -57,8 +45,12 @@ from Expts import *
 # We put together our Model from the experiments and networks
 m = Model([expt1], [base_net, growth_net])
 params = m.get_params()
-print params
-print params.items()
+for id, val in params.items():
+    print id
+    print val
+
+    m.AddResidual(Residuals.PriorInLog('prior_on_%s' % id, id, scipy.log(val),
+                                       scipy.log(10.0)))
 print 'Initial cost:', m.cost(params)
 Plotting.figure(2)
 Plotting.plot_model_results(m, loc='upper right')
@@ -71,11 +63,6 @@ Plotting.title('Before fitting')
 #  with 95% probility, between val/100 and val * 100.
 # (These are quite tight priors. Real applications (with more constraining data)
 #  will probably want looser priors.)
-print params.items()
-for id, val in params.items():
-    print val
-    m.AddResidual(Residuals.PriorInLog('prior_on_%s' % id, id, scipy.log(val), 
-                                       scipy.log(10)))
 
 # And now we'll optimize. Note that optimization can be tricky. One needs to
 #  be in the right ball-park before the local routines can help much.
@@ -83,10 +70,14 @@ for id, val in params.items():
 #  they have different strengths and may head different places.
 
 # First we'll try Nelder-Mead
+print params
 pmin1 = Optimization.fmin_log_params(m, params, xtol=1e-2)
 # Then we run Levenburg-Marquardt
+print pmin1
 params = Optimization.leastsq_log_params(m, pmin1)
+print params
 # We save our parameter values, the reload them.
+print params
 Utility.save(params, 'min_params.bp')
 
 params = Utility.load('min_params.bp')
@@ -110,7 +101,16 @@ evals, evects = Utility.eig(hess)
 Plotting.figure(4)
 Plotting.plot_eigvals(evals)
 Plotting.figure(5)
-Plotting.plot_eigvect(evects[:,0], params.keys())
+Plotting.plot_eigvect(evects[:,0],params.keys())
+Plotting.figure()
+print len(evects)
+for number in range(9):
+    print evects[:, number]
+    Plotting.subplot(3,3,number+1)
+    print len(evects[:,number])
+    print len(params.keys())
+    Plotting.plot_eigvect(evects[:, number], params.keys())
+
 
 # Now we'll build an ensemble of parameters.
 # Make sure we run at full speed