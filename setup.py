9113cee11ed646178baf6e5520674976d6e5a64c
Ryan Gutenkunst <rgutenk@email.arizona.edu>
2017-08-03 15:27:33 -0700

    Further simplify setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,15 @@
-# Importing these adds a 'bdist_mpkg' option that allows building binary packages on OS X.
-try:
-    import setuptools
-    import bdist_mpkg
-except ImportError:
-    pass
-
 import numpy.distutils.core as core
-
 daskr = core.Extension(name = 'SloppyCell._daskr',
                        sources = ['SloppyCell/daskr.pyf',
                                   'ddaskr/ddaskr.c',
                                   'ddaskr/daux.c',
                                   'ddaskr/dlinpk.c',
                                   'ddaskr/ddaskr_types.h'])
-
 misc_c = core.Extension(name = 'SloppyCell.misc_c',
                         sources = ['SloppyCell/misc_c.c',
                                    'SloppyCell/misc_c.pyf'])
 
+# Collect files needed for dynamic compilation
 rxn_data_files = ['SloppyCell/ReactionNetworks/{0}'.format(_) for _
                   in ['mtrand.c', 'mtrand.h', 'f2py_signatures.pyf',
                       'f2py_signatures_no_derivs.pyf']]
@@ -45,6 +37,7 @@ core.setup(name='SloppyCell',
                      'SloppyCell.ReactionNetworks',
                      'SloppyCell.Vandermonde'],
            ext_modules = [daskr, misc_c],
+           # Ensure files needed for dynamic compilation are installed
            zip_safe=False,
            data_files=[('SloppyCell/ReactionNetworks', rxn_data_files)],
            include_package_data = True,
