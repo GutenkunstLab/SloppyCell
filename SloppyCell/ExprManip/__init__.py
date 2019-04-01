453edcfbb99e921f13d269b20ab12c5390c442e1
Ryan Gutenkunst <rgutenk@email.arizona.edu>
2019-04-01 11:12:21 -0700

    Imports to py3

--- SloppyCell/ExprManip/__init__.py
+++ SloppyCell/ExprManip/__init__.py
@@ -1,14 +1,9 @@
-import AST
-import Differentiation
-import Extraction
-import Py2TeX
-import Simplify
-import Substitution
+from . import AST, Differentiation, Extraction, Py2TeX, Simplify, Substitution
 
-from AST import strip_parse, ast2str
-from Differentiation import load_derivs, save_derivs, diff_expr
-from Extraction import extract_vars, extract_funcs, extract_comps
-from Py2TeX import expr2TeX
-from Simplify import simplify_expr
-from Substitution import sub_for_var, sub_for_func, sub_for_vars, sub_for_comps
-from Substitution import make_c_compatible 
+from .AST import strip_parse, ast2str
+from .Differentiation import load_derivs, save_derivs, diff_expr
+from .Extraction import extract_vars, extract_funcs, extract_comps
+from .Py2TeX import expr2TeX
+from .Simplify import simplify_expr
+from .Substitution import sub_for_var, sub_for_func, sub_for_vars, sub_for_comps
+from .Substitution import make_c_compatible 
